7cde3521fa1ff5d531a26ac6290ed8322e61cebe
danishdev07 <danish.dev07@gmail.com>
2024-05-27 14:15:22 +0530

    signin and login component created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,8 @@ import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { SidenavComponent } from './sidenav/sidenav.component';
+import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +16,9 @@ import { SidenavComponent } from './sidenav/sidenav.component';
     HeaderComponent,
     FooterComponent,
     DashboardComponent,
-    SidenavComponent
+    SidenavComponent,
+    LoginComponent,
+    SignupComponent
   ],
   imports: [
     BrowserModule,
